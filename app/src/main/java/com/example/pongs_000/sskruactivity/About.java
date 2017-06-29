1c2d8c1e0fb85f781f5586aae64a7f89d140a313
Mr.sonthaya <son_5212@hotmail.com>
2017-06-29 11:54:35 +0700

    About.java open to page facebook

--- app/src/main/java/com/example/pongs_000/sskruactivity/About.java
+++ app/src/main/java/com/example/pongs_000/sskruactivity/About.java
@@ -1,7 +1,12 @@
 package com.example.pongs_000.sskruactivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 /**
  * Created by sonthaya-csit on 4/18/17.
@@ -13,5 +18,25 @@ public class About extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about);
+
+        getSupportActionBar().setTitle(" ทีมพัฒนา");
+        getSupportActionBar().setSubtitle(" ระบบตรวจเช็คกิจกรรม");
+
+        TextView csit = (TextView)findViewById(R.id.csit56);
+        csit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent facebookIntent = new Intent(About.this ,Fbcsit.class );
+                startActivity(facebookIntent);
+            }
+        });
+        TextView csit1 = (TextView)findViewById(R.id.textView5);
+        csit1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent facebookIntent = new Intent(About.this ,Fbcsit.class );
+                startActivity(facebookIntent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/pongs_000/sskruactivity/Fbcsit.java
@@ -0,0 +1,44 @@
+package com.example.pongs_000.sskruactivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by IT03 on 29/6/2560.
+ */
+
+public class Fbcsit extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+
+        Intent facebookIntent = getOpenFacebookIntent(this);
+        startActivity(facebookIntent);
+
+//        TextView csit = (TextView)findViewById(R.id.csit56);
+//        csit.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent facebookIntent = getOpenFacebookIntent(this);
+//                startActivity(facebookIntent);
+//            }
+//        });
+    }
+    public static Intent getOpenFacebookIntent(Context context) {
+
+        try {
+            context.getPackageManager()
+                    .getPackageInfo("com.facebook.katana", 0); //Checks if FB is even installed.
+            return new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("fb://page/1131491843567869")); //Trys to make intent with FB's URI
+        } catch (Exception e) {
+            return new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://www.facebook.com/csit56")); //catches and opens a url to the desired page
+        }
+    }
+}
